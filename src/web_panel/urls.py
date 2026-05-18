dd6986341638ffc239d82e33b3ef1cd16ccdc6a5
DudarDev <yaroslavupwork97@gmail.com>
2026-05-18 13:32:19 +0000

    feat(infra): introduce postgresql, configure log rotation, and fix 404 routing

--- src/web_panel/urls.py
+++ src/web_panel/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path
-from bot_monitor.views import dashboard_view
-from .api import api
+from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('dashboard/', dashboard_view, name='dashboard'),
     path('admin/', admin.site.urls),
-    path('api/', api.urls),
+    # Підключаємо урли твого додатку bot_monitor
+    path('bot_monitor/', include('bot_monitor.urls')),
+    # Якщо користувач заходить просто на IP-адресу, кидаємо його в адмінку або на дашборд
+    path('', RedirectView.as_view(url='/admin/', permanent=False)),
 ]

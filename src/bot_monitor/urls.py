66a07494db229fead76725a7c50326a0e651031e
DudarDev <yaroslavupwork97@gmail.com>
2026-05-18 14:58:01 +0000

    fix(dashboard): use rock-solid django url tags

--- src/bot_monitor/urls.py
+++ src/bot_monitor/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     # Ваш основний дашборд
     path('dashboard/', views.dashboard_view, name='dashboard'),
-    
+
     # API для JS
     path('api/status/', views.api_bot_status, name='api_bot_status'),
-    path('api/control', views.api_bot_control, name='api_bot_control'), # У JS ви викликали /api/control (без слеша в кінці)
+    path('api/control/', views.api_bot_control, name='api_bot_control'), # Додав слеш тут!
 ]
